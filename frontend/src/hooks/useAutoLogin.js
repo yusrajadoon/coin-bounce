52df55fb4d1ac04d129675f667868e78ea576789
yusra jadoon <yusrajadoon586@gmail.com>
2025-04-16 12:41:14 +0500

    updated

--- frontend/src/hooks/useAutoLogin.js
+++ frontend/src/hooks/useAutoLogin.js
@@ -5,11 +5,9 @@ import axios from "axios";
 
 function useAutoLogin() {
   const [loading, setLoading] = useState(true);
-
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // IIFE
     (async function autoLoginApiCall() {
       try {
         const response = await axios.get(
@@ -20,18 +18,16 @@ function useAutoLogin() {
         );
 
         if (response.status === 200) {
-          // 1. setUser
           const user = {
             _id: response.data.user._id,
             email: response.data.user.email,
             username: response.data.user.username,
             auth: response.data.auth,
           };
-
           dispatch(setUser(user));
         }
       } catch (error) {
-        //
+        console.error("Auto login failed", error); // optional
       } finally {
         setLoading(false);
       }
